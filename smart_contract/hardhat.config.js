7c1a79623520835c8a4c3239546463171a44ba79
sobbey99 <sobbey99@gmail.com>
2023-06-19 22:23:44 +0300

    Transaction base smart contract hardhat wafle

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-sepolia.g.alchemy.com/v2/wEdbv_KpwQ9lVc25JKWnd150JnhNhwi9
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/wEdbv_KpwQ9lVc25JKWnd150JnhNhwi9",
+      accounts: [
+        "adabe4f2d31a59d0da6578872ace71c950506cbb6ff5ba7cbf073077cfc8610b",
+      ],
+    },
+  },
 };
